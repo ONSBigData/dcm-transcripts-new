35a5ac51ca35bfb4f2ebdf09d433cd767ab41827
Huw Fulcher <huw.fulcher@ons.gov.uk>
2019-11-19 11:24:10 +0000

    Added icsi loader

--- recordings/icsi.py
+++ recordings/icsi.py
@@ -5,7 +5,38 @@ From http://groups.inf.ed.ac.uk/ami/icsi/download/
 from common import *
 from recordings.recording import *
 import pandas as pd
-import recordings.prep.segmenting_helper as sh
+import os
 
-FPATH = from_data_root('recordings/icsi/')[:-1]
-TRANSCRIPT_FPATH = f'{FPATH}/t'
+
+DIR = from_data_root('recordings/icsi')
+
+
+def get_fnames():
+    return [
+        f.replace('.wav', '')
+        for f in os.listdir(DIR) if f.endswith('.wav')
+    ]
+
+
+def prep():
+    pass
+
+
+def load_all():
+    return [load(fname) for fname in get_fnames()]
+
+
+def load(fname):
+    r = Recording()
+
+    r.audio_fpath = f'{DIR}/{fname}.wav'
+
+    r.structured_transcript = None
+    r.no_speakers = None
+
+    return r
+
+
+if __name__ == '__main__':
+    prep()
+    print(load_all())
